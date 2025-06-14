4ec048b63080a1fdf3c625e9fdfd23758c7c332c
GeorgeImanuel <georgeimanuel123@gmail.com>
2025-06-14 12:47:05 +0700

    Delete src directory

--- src/model/PricedItem.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public interface PricedItem {
-    double getPrice();
-}
--- src/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Constants {
-    public static final String ROLE_ADMIN = "admin";
-    public static final String ROLE_CUSTOMER = "customer";
-    public static final String DB_URL = "jdbc:mysql://localhost:3306/db_appmakanan";
-    public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = ""; // sesuaikan
-}
